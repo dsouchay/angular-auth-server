f32783ec6fae85096c698ef329ffc4e24cf403c1
Dania Souchay <dania.souchay@webfg.com>
2022-05-30 17:38:22 +0200

    ejecutado el build para sacar la app de angular

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const { application } = require("express");
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+
+
 const { dbConnection } = require("./db/config");
 require('dotenv').config();
 // Crear el servidor / aplicacion de express
@@ -16,6 +19,14 @@ app.use(cors());
 app.use(express.json());
 //Rutas
 app.use('/api/auth', require('./routes/auth'));
+
+//Manejar demás rutas
+app.get('*', (req,res)=>{
+  res.sendFile(path.resolve(path.resolve(__dirname,'public/index.html')));
+
+})
+
+
 app.listen(process.env.PORT, () => {
   console.log(`Servidor corriendo en el puerto ${ process.env.PORT }`);
 })
